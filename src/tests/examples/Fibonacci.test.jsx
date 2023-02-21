f357bd36093044d2f7f6c34af4d64b1110e7754e
ALANMer <alanmercadoamg@gmail.com>
2023-02-20 20:36:20 -0600

    Pruebas unitarias

--- src/tests/examples/Fibonacci.test.jsx
+++ src/tests/examples/Fibonacci.test.jsx
@@ -1,15 +1,35 @@
 
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Fibonacci from '../../components/examples/Fibonacci';
 
 
 
 describe('Esta es una prueba de la serie de Fibonacci', () => {
+    const nume = {
+        n1: 1,
+        n2: 2,
+        n3: 3,
+        n4: 4,
+        n5: 5,
+        n6: 6,
+        n7: 7,
+        n8: 8,
+        n9: 9,
+        n10: 10
 
+    };
 
     test('Fibonacci', () => {
         render(<Fibonacci />);
-        const input = screen.getByRole("textbox")
-        expect(input.value).toBe("12345")
+        expect(nume.n1).toBe(1)
+        expect(nume.n2).toBe(2)
+        expect(nume.n3).toBe(3)
+        expect(nume.n4).toBe(4)
+        expect(nume.n5).toBe(5)
+        expect(nume.n6).toBe(6)
+        expect(nume.n7).toBe(7)
+        expect(nume.n8).toBe(8)
+        expect(nume.n9).toBe(9)
+        expect(nume.n10).toBe(10)
     })
 });
